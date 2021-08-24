8cc8d7a343eb49e0fa77d172f1cfeeed76aa16d3
Nicholas Jacob <nicholas.jacob@guitarcenter.com>
2021-08-23 18:07:05 -0700

    Configuring and styling a TextInput

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
 import Card from '../components/Card';
+import Input from '../components/Input';
 import Colors from '../constants/colors';
 
 const StartGameScreen = (props) => {
@@ -9,10 +11,21 @@ const StartGameScreen = (props) => {
       <Text style={styles.title}>Start a New Game!</Text>
       <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
-        <TextInput />
+        <Input
+          style={styles.input}
+          blurOnSubmit
+          autoCapitalize='none'
+          autoCorrect={false}
+          keyboardType='number-pad'
+          maxLength={2}
+        />
         <View style={styles.buttonContainer}>
-          <View style={styles.button}><Button title='Reset' onPress={() => {}} color={Colors.accent} /></View>
-          <View style={styles.button}><Button title='Confirm' onPress={() => {}} color={Colors.primary} /></View>
+          <View style={styles.button}>
+            <Button title='Reset' onPress={() => {}} color={Colors.accent} />
+          </View>
+          <View style={styles.button}>
+            <Button title='Confirm' onPress={() => {}} color={Colors.primary} />
+          </View>
         </View>
       </Card>
     </View>
@@ -32,7 +45,7 @@ const styles = StyleSheet.create({
   inputContainer: {
     width: 300,
     maxWidth: '80%',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   buttonContainer: {
     flexDirection: 'row',
@@ -41,8 +54,12 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
   },
   button: {
-      width: 100
-  }
+    width: 100,
+  },
+  input: {
+    width: 50,
+    textAlign: 'center',
+  },
 });
 
 export default StartGameScreen;
