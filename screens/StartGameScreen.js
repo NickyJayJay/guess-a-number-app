52f18cbb4ff6751d13019a904b83c1d39a9172a0
Nick Jacob <njacob341@gmail.com>
2021-08-25 17:05:40 -0700

    A synthetic style 'cascade': custom wrapper components and global styles

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -12,6 +12,7 @@ import {
 import Card from '../components/Card';
 import Input from '../components/Input';
 import NumberContainer from '../components/NumberContainer';
+import BodyText from '../components/BodyText';
 import Colors from '../constants/colors';
 
 const StartGameScreen = (props) => {
@@ -49,7 +50,7 @@ const StartGameScreen = (props) => {
   if (confirmed) {
     confirmedOutput = (
       <Card style={styles.summaryContainer}>
-        <Text>You selected</Text>
+        <BodyText>You selected</BodyText>
         <NumberContainer>{selectedNumber}</NumberContainer>
         <Button title='START GAME' onPress={() => props.onStartGame(selectedNumber)} />
       </Card>
@@ -65,7 +66,7 @@ const StartGameScreen = (props) => {
       <View style={styles.screen}>
         <Text style={styles.title}>Start a New Game!</Text>
         <Card style={styles.inputContainer}>
-          <Text>Select a Number</Text>
+          <BodyText>Select a Number</BodyText>
           <Input
             style={styles.input}
             blurOnSubmit
