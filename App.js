2b02c0010dc7c09f83826080619b6447b15b69bb
Nick Jacob <njacob341@gmail.com>
2021-08-25 18:43:00 -0700

    Adding local images

--- App.js
+++ App.js
@@ -46,13 +46,25 @@ export default function App() {
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
 
+  content = (
+    <GameOverScreen
+      roundsNumber={1}
+      userNumber={1}
+      onRestart={configureNewGameHandler}
+    />
+  );
+
   if (userNumber && guessRounds <= 0) {
     content = (
       <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
     );
   } else if (guessRounds > 0) {
     content = (
-      <GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} onRestart={configureNewGameHandler} />
+      <GameOverScreen
+        roundsNumber={guessRounds}
+        userNumber={userNumber}
+        onRestart={configureNewGameHandler}
+      />
     );
   }
 
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
 
 import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <BodyText>The Game is Over!</BodyText>
+      <TitleText>The Game is Over!</TitleText>
+      <Image source={require('../assets/success.png')} />
       <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
       <BodyText>Number was: {props.userNumber}</BodyText>
       <Button title='NEW GAME' onPress={props.onRestart} />
